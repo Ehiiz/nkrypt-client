73cbe126766441eb902b0e1bda542aa70b8b7808
Ehiiz <62038343+Ehiiz@users.noreply.github.com>
2022-06-12 20:26:11 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    "*",
     createProxyMiddleware({
       target: 'https://sleepy-escarpment-55626.herokuapp.com',
       changeOrigin: true,
