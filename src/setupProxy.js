9921b986557450e99fb13c39ba8763a92a71b10b
Ehiiz <62038343+Ehiiz@users.noreply.github.com>
2022-06-12 20:14:38 +0530

    added * to apply on all routes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
+    "*",
     createProxyMiddleware({
       target: 'https://sleepy-escarpment-55626.herokuapp.com',
       changeOrigin: true,
