926c924989d01785f279b3e4ec03dacb4dde0a85
Ehiiz <62038343+Ehiiz@users.noreply.github.com>
2022-06-13 19:57:38 +0530

    Update Createkrypt.jsx
    replaced user with ID

--- src/pages/Createkrypt.jsx
+++ src/pages/Createkrypt.jsx
@@ -400,7 +400,7 @@ const handleDelete = (e) =>{
                 notification={navcolor.notification}
                 profile={navcolor.profile}
                 search={navcolor.search}
-                user={data.user._id}
+                user={"62a374fcafcbd93ed7956d44"}
             />
 
       </div>
